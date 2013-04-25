a2b565fbfa9359e34a0d9ffe26a151eed4d498c1
DTrejo <david.daniel.trejo@gmail.com>
2013-04-25 11:39:03 -0700

    demo: cache==0, button to drive clicks

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var ErrorPage = require('error-page');
 var ecstatic = require('ecstatic');
 
 var PUBLIC = path.join(__dirname, 'static');
-var PORT = 8888;
+var PORT = 9999;
 
-var static = ecstatic({ root: PUBLIC, autoIndex: true });
+var static = ecstatic({
+  root: PUBLIC, autoIndex: true, cache: 1//, defaultExt: 'html'
+});
 
 http.createServer(function(req, res) {
   res.error = ErrorPage(req, res);
